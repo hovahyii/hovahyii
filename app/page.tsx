f498e7a9828d38e6444c72926f59d84aa60a5891
hovahyii <hovahyii@gmail.com>
2024-03-27 09:31:53 +0800

    update buy me a coffee message
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -15,8 +15,7 @@ export default function Home() {
   {/* Cards Section */}
   <ProjectPage />
 
-  <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="hovahyii" data-description="Support me on Buy me a coffee!" data-message="🌟 Do my creations kindle your spirit, stir your mind? 🌟
-" data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+  <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="hovahyii" data-description="Support me on Buy me a coffee!" data-message="Do my creations kindle your spirit, stir your mind? Buy me a coffee ☕ then 🥰..." data-color="#FF813F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 </div>
     <Footer />
 </>
