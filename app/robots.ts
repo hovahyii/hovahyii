b1a4c916c82bd29f4587fc3110aaa42f04a6a391
hovahyii <hovahyii@gmail.com>
2024-05-03 23:55:09 +0800

    added sitemap.ts
    Signed-off-by: hovahyii <hovahyii@gmail.com>

---
+++ app/robots.ts
@@ -0,0 +1,19 @@
+
+import type { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: [
+      {
+        userAgent: 'Googlebot',
+        allow: ['/'],
+        disallow: '/private/',
+      },
+      {
+        userAgent: ['Applebot', 'Bingbot'],
+        disallow: ['/'],
+      },
+    ],
+    sitemap: 'https://hovahyii.vercel.app/sitemap.xml',
+  }
+}
