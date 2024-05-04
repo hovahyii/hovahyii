dab66966ffdb936c3e17be66fa98b89c3f1ae5fc
hovahyii <hovahyii@gmail.com>
2024-05-04 18:55:16 +0800

    update sitemap
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,7 +5,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://hovahyii.vercel.app/',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: 'daily',
       priority: 1,
     },
   ]
