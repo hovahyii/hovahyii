5a2f092694ed5a1516046b41800239db79f90dc8
hovahyii <hovahyii@gmail.com>
2024-05-28 02:48:43 +0800

    adjust alignment for badge task status
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- components/projects.tsx
+++ components/projects.tsx
@@ -27,48 +27,46 @@ const ProjectPage: React.FC = () => {
         <h1 className="text-4xl font-bold mb-4">Projects</h1>
         <div className="flex space-x-4 mb-2">
           <Badge
-  className={selectedFilter === "Web" ? "selected" : ""}
-  onClick={() => handleFilterClick("Web")}
+            className={selectedFilter === "Web" ? "selected" : ""}
+            onClick={() => handleFilterClick("Web")}
           >
             Web
           </Badge>
           <Badge
             className={selectedFilter === "Engineering" ? "selected" : ""}
-
             onClick={() => handleFilterClick("Engineering")}
           >
             Engineering
           </Badge>
           <Badge
-  className={selectedFilter === "IoT" ? "selected" : ""}
-  onClick={() => handleFilterClick("IoT")}
+            className={selectedFilter === "IoT" ? "selected" : ""}
+            onClick={() => handleFilterClick("IoT")}
           >
             IoT
           </Badge>
           <Badge
-  className={selectedFilter === "3D Modeling" ? "selected" : ""}
-  onClick={() => handleFilterClick("3D Modeling")}
+            className={selectedFilter === "3D Modeling" ? "selected" : ""}
+            onClick={() => handleFilterClick("3D Modeling")}
           >
             3D Modeling
           </Badge>
         </div>
       </div>
       <div className="grid grid-cols-1 lg:mt-6 lg:ml-6 lg:grid-cols-2 gap-4">
-      {projectData
-    .filter(
-      (project) => !selectedFilter || project.filter.includes(selectedFilter)
-    )
+        {projectData
+          .filter(
+            (project) => !selectedFilter || project.filter.includes(selectedFilter)
+          )
           .map((project, index) => (
             <Card key={index} className="w-full transition-shadow duration-300 hover:shadow-lg">
               <Link href={project.website} target="_blank" rel="noopener noreferrer" className="block">
-                <CardHeader className="flex justify-start items-start ">
+                <CardHeader className="flex justify-start items-start">
                   <div className="flex items-center">
-                    <Image src={project.logo} alt={project.name}  width={32} height={32}  className="mr-2" />
+                    <Image src={project.logo} alt={project.name} width={32} height={32} className="mr-2" />
                     <CardTitle className="text-sm truncate">{project.name}</CardTitle>
-                  
                     <Badge
                       variant="default"
-                      className={`flex absolute ml-56 md:ml-80 lg:ml-64 text-white ${
+                      className={`flex absolute ml-56 md:ml-72 text-white ${
                         project.status === "Completed"
                           ? "bg-green-500"
                           : project.status === "On-going"
@@ -79,24 +77,25 @@ const ProjectPage: React.FC = () => {
                           : ""
                       }`}
                     >
-                                            {project.status}
-
+                      {project.status}
                     </Badge>
-            
                   </div>
                   {project.revenue && (
                     <Badge variant="default" className="bg-gray-200 text-black">
                       {project.revenue}
                     </Badge>
                   )}
+                  {project.award && (
+                    <Badge variant="default" className="bg-yellow-500 text-white ">
+                      {project.award}
+                    </Badge>
+                  )}
                 </CardHeader>
-        
                 <CardContent>
-            
                   <CardDescription>{project.description}</CardDescription>
                 </CardContent>
                 <CardFooter className="justify-between">
-                  <div className="text-xs" dangerouslySetInnerHTML={{__html:project.type}} />
+                  <div className="text-xs" dangerouslySetInnerHTML={{ __html: project.type }} />
                 </CardFooter>
               </Link>
             </Card>
