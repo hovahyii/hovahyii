58a1280a341bfd85895e0bc725a7306b1cc2131a
hovahyii <hovahyii@gmail.com>
2024-06-02 22:18:53 +0800

    update VCARD
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- components/ContactVCFComponent.tsx
+++ components/ContactVCFComponent.tsx
@@ -10,16 +10,16 @@ interface ContactVCFProps {
 const ContactVCFComponent: React.FC<ContactVCFProps> = ({ isDarkMode }) => {
   const handleDownloadVCF = () => {
     const vcfData = `
-        BEGIN:VCARD
-        VERSION:3.0
-        FN:Jehovah Yii Zui Hon
-        TEL;TYPE=WORK,VOICE:01110501375
-        EMAIL;TYPE=PREF,INTERNET:hovahyii@gmail.com
-        ORG:Huawei Technologies Malaysia Sdn. Bhd
-        TITLE:RF Engineer
-        URL;TYPE=WORK:https://hovahyii.vercel.app
-        URL;TYPE=WORK:https://tapnex-card.vercel.app
-        END:VCARD
+BEGIN:VCARD
+VERSION:3.0
+FN:Jehovah Yii Zui Hon
+TEL;TYPE=WORK,VOICE:01110501375
+EMAIL;TYPE=PREF,INTERNET:hovahyii@gmail.com
+ORG:Huawei Technologies Malaysia Sdn. Bhd
+TITLE:RF Engineer
+URL;WORK:https://hovahyii.vercel.app
+URL;WORK:https://tapnex-card.vercel.app
+END:VCARD
     `;
     const blob = new Blob([vcfData], { type: 'text/vcard' });
     const url = URL.createObjectURL(blob);
@@ -31,7 +31,7 @@ const ContactVCFComponent: React.FC<ContactVCFProps> = ({ isDarkMode }) => {
 
   return (
     <button onClick={handleDownloadVCF} className={`flex items-center dark:text-white  space-x-2 ${isDarkMode ? 'text-white' : 'text-gray-600'} hover:text-gray-800`}>
-        <MdDownloadForOffline size="1.5em"/>
+      <MdDownloadForOffline size="1.5em"/>
       <span>Save Contact</span>
     </button>
   );
