4cec84549beef945ba49e5674ce7c30f614a1e57
hovahyii <hovahyii@gmail.com>
2024-07-03 20:00:58 +0800

    update meta
    Signed-off-by: hovahyii <hovahyii@gmail.com>

--- app/last-paper/page.tsx
+++ app/last-paper/page.tsx
@@ -2,8 +2,8 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import Head from 'next/head';
 import Image from 'next/image';
+
 
 const CountdownPage = () => {
   const calculateTimeLeft = () => {
@@ -42,23 +42,20 @@ const CountdownPage = () => {
 
   return (
     <div>
-      <Head>
-        <title>My Last Paper - Conclude My 4 Years of Studies at UMPSA</title>
-      </Head>
-      <main className="text-center p-12">
-        <h1 className="text-4xl font-bold">My Last Paper</h1>
-        <h2 className="text-3xl mt-4">Conclude My 4 Years of Studies at UMPSA (Universiti Malaysia Pahang Al-Sultan Abdullah)</h2>
-        <h2 className="text-3xl mt-2">Wish me luck</h2>
+      <main className="text-center p-6 md:p-12">
+        <h1 className="text-3xl md:text-4xl font-bold">My Last Paper</h1>
+        <h2 className="text-xl md:text-3xl mt-4">Conclude My 4 Years of Studies at UMPSA (Universiti Malaysia Pahang Al-Sultan Abdullah)</h2>
+        <h2 className="text-xl md:text-3xl mt-2">Wish me luck</h2>
         <div className="mb-8"></div>
-        <h3 className="text-4xl font-bold">Countdown</h3>
-        <div className="flex justify-center text-5xl mt-8">
-          <div className="mx-4">{timeLeft.days} Days</div>
-          <div className="mx-4">{timeLeft.hours} Hours</div>
-          <div className="mx-4">{timeLeft.minutes} Minutes</div>
-          <div className="mx-4">{timeLeft.seconds} Seconds</div>
+        <h3 className="text-2xl md:text-4xl font-bold">Countdown</h3>
+        <div className="flex flex-col md:flex-row justify-center text-4xl md:text-5xl mt-8">
+          <div className="mx-2 md:mx-4">{timeLeft.days} Days</div>
+          <div className="mx-2 md:mx-4">{timeLeft.hours} Hours</div>
+          <div className="mx-2 md:mx-4">{timeLeft.minutes} Minutes</div>
+          <div className="mx-2 md:mx-4">{timeLeft.seconds} Seconds</div>
         </div>
-        <div className="flex justify-center mt-12">
-          <Image src="/graduation.png" alt="Graduation" width={600} height={400} />
+        <div className="flex justify-center mt-8 md:mt-12">
+          <Image src="/graduation.png" alt="Graduation" width={600} height={400} className="w-full md:w-auto" />
         </div>
       </main>
     </div>
