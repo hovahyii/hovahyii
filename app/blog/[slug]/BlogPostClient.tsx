cc36e9202290caf125cc05e0755d34be8f8fed04
Hovah <hovahyii@gmail.com>
2025-05-08 15:10:58 +0800

    update blog
    Signed-off-by: Hovah <hovahyii@gmail.com>

--- app/blog/[slug]/BlogPostClient.tsx
+++ app/blog/[slug]/BlogPostClient.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { BlogPost } from './page';
+import { BlogPost } from '../data/blogPosts';
 
 interface BlogPostClientProps {
   slug: string;
